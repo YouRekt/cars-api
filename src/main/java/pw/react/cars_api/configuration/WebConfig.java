b8029e32484b8df9aa283d40c552ae99aff9abcc
YouRekt <czarnecki.jurko@gmail.com>
2025-02-04 04:10:44 +0100

    feat: Added the ability for admin authorization

---
+++ src/main/java/pw/react/cars_api/configuration/WebConfig.java
@@ -0,0 +1,16 @@
+package pw.react.cars_api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173")  // Specify the allowed origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE").allowCredentials(true)  // Allow cookies and credentials
+                .allowedHeaders("*");   // Allow all headers (or specify required ones)
+    }
+}
